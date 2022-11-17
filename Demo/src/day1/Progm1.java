d6dd03f20245ccdf84f74bc01249c315218e8160
Hp <Hp@DESKTOP-MEHK842>
2022-11-16 23:26:49 -0800

    first commit

---
+++ Demo/src/day1/Progm1.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Progm1 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to day 1");
+	}
+
+}
---
+++ Demo/src/day1/prog2.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class prog2 {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		System.out.println(a+b);
+
+	}
+
+}
